eab1ad4b78890ea0efb64b236ec6d95cae7727c5
tars003 <sharmaajay2kuu@gmail.com>
2022-11-07 14:44:41 +0530

    design improvements

--- src/components/SingleUnit.js
+++ src/components/SingleUnit.js
@@ -4,6 +4,7 @@ import { CSVLink, CSVDownload } from "react-csv";
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 const rootUrl = "https://bio-digester-server.herokuapp.com";
 
@@ -45,21 +46,23 @@ const SingleUnit = (props) => {
                 <Grid container style={{marginBottom: '10px'}} spacing={2}>
                     <Grid item xs={6}>
                         <div className="container" >
-                            <table className="table">
+                            {/* DATE TIME TABLE */}
+                            <table style={{width : "80%"}}  className="table">
                                 <tbody>
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
-                                            Date </td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td style={{color: "black"}} >
+                                            DATE </td>
+                                        <td  style={{color: "white", backgroundColor: "rgb(45, 203, 123)"}} >
                                             {currSerialRes.serverDate}</td>
 
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
-                                            Time  </td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td style={{color: "black"}} >
+                                            TIME  </td>
+                                        <td  style={{color: "white", backgroundColor: "rgb(45, 203, 123)"}} >
                                         {currSerialRes.serverTime}</td>
                                     </tr>
                                 </tbody>
                             </table>
+                            {/* DATE TIME TABLE */}
                         </div>
                     </Grid>
                 </Grid>
@@ -73,37 +76,37 @@ const SingleUnit = (props) => {
                             <table className="table">
                                 <tbody>
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             Temp 1</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.temp1}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             Temp 2</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.temp2}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             Temp 3</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.temp3}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             Temp 4</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.temp4}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             Temp 5</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.temp5}</td>
                                     </tr>
                                 </tbody>
@@ -117,37 +120,37 @@ const SingleUnit = (props) => {
                             <table className="table">
                                 <tbody>
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             pH 1</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                         {currSerialRes.ph1}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             pH 2</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.ph2}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             pH 3</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.ph3}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             pH 4</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.ph4}</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black"}} >
                                             pH 5</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                             {currSerialRes.ph5}</td>
                                     </tr>
                                 </tbody>
@@ -158,28 +161,44 @@ const SingleUnit = (props) => {
 
                     <Grid item xs={6}>
                         <div className="container" >
-                            {/* MODBUS  TABLE */}
+                            {/* FLOW  TABLE */}
                             <table className="table">
                                 <tbody>
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "Gray"}} >
-                                            Energy </td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
-                                        {currSerialRes.energy} KWH</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black", fontWeight: "bold"}}>
                                             Flow Rate </td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                         {currSerialRes.flow} LPM</td>
                                     </tr>
 
                                     <tr>
-                                        <td style={{backgroundColor: "#00FFFF", color: "black"}} >
+                                        <td style={{color: "black", fontWeight: "bold"}}>
                                             Flow Volume</td>
-                                        <td  style={{backgroundColor: "rgb(228, 58, 58)", color: "white"}} >
+                                        <td  style={{color: "black"}} >
                                         {currSerialRes.flowVolume} L</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            {/* FLOW  TABLE */}
+                        </div>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <div className="container" >
+                            {/* MODBUS  TABLE */}
+                            <table className="table">
+                                <tbody>
+                                    <tr>
+                                        <td style={{color: "black", fontWeight: "bold"}} >
+                                            Energy </td>
+                                        <td  style={{color: "black"}} >
+                                        {currSerialRes.energy} KWH</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{color: "black", fontWeight: "bold"}} >
+                                            Humidity </td>
+                                        <td  style={{color: "black"}} >
+                                        {currSerialRes.rh}  % </td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -188,8 +207,20 @@ const SingleUnit = (props) => {
                     </Grid>
                 </Grid>
 
+                <Container fixed style={{marginLeft: '-25px'}}>
+                    <h2> Download Historical Data</h2>
+                </Container>
+
                 {/* CSV DOWNLAOAD BUTTON */}
-                <CSVLink  filename={"bio-digester.csv"} data={allSerialRes}>Download me</CSVLink>
+                <Grid style={{marginTop: '10px', marginLeft: '-10px'}} container spacing={0}>
+                    <Grid item xs={6}>
+                        <Button className="csv-link"   style={{ margin: '10px', minWidth: '300px' }}variant="contained"><CSVLink  filename={"bio-digester.csv"} data={allSerialRes}>DOWNLOAD LAST 3 DAYS</CSVLink></Button>
+                    </Grid>
+                    <Grid item xs={6}></Grid>
+                    <Grid item xs={6}>
+                        <Button className="csv-link"  style={{ margin: '10px', minWidth: '300px' }}variant="contained"><CSVLink  filename={"bio-digester.csv"} data={allSerialRes}>DOWNLOAD LAST 7 DAYS</CSVLink></Button>
+                    </Grid>
+                </Grid>
             </Container>
         </>
     );
